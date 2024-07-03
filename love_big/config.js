77412da38af22291b4c43e8b7667b099072c0912
Nguyen Van Tuyen <nguyenvantuyen1009@gmail.com>
2024-07-03 23:22:12 +0700

    ud

--- love_big/config.js
+++ love_big/config.js
@@ -7,7 +7,10 @@ var month = date.getMonth() + 1;
 var eventName = 'Chúc Mừng '+day+'-'+month;
 const titleCard = 'Tặng Bé Yêu';
 var contentCard = 'Chúc công chúa của anh '+day+'/'+month+' tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay';
-var images = ['01.png', '02.png', '03.png', '04.png', '05.png', '06.png', '07.png', '08.png', '09.png', '10.png', '11.png', '12.png', '13.png', '14.png', '15.png', '16.png', '17.png'];
+var images = ['01.png', '02.png', '03.png', '04.png', '05.png', '06.png'
+    , '07.png', '08.png', '09.png', '10.png', '11.png', '12.png', '13.png', '14.png', '15.png', '16.png', '17.png'
+    , '18.png', '18.png', '20.png', '21.png', '22.png', '23.png',
+];
 var randomIndex = Math.floor(Math.random() * images.length);
 const giftImage = '../Love/images/'+ images[randomIndex];
 const base64 = '';
